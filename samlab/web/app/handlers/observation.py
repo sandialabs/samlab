4ec9259fd74028901173b87e8759246cf7446fbe
Timothy M. Shead <tshead@sandia.gov>
2018-07-18 10:36:49 -0600

    Remove unused code.

--- samlab/web/app/handlers/observation.py
+++ samlab/web/app/handlers/observation.py
@@ -23,34 +23,6 @@ from samlab.web.app import application, socketio, require_auth, require_permissi
 from samlab.web.app.database import database, fs
 
 
-@application.route("/observations")
-@require_auth
-def get_observations():
-    require_permissions(["read"])
-
-    search = flask.request.args.get("search", "")
-    if search:
-        observations = samlab.observation.expand(database, samlab.object.search(database, "observations", search))
-    else:
-        observations = list(database.observations.find({}))
-
-    sort = flask.request.args.get("sort", "tags")
-    if sort == "id":
-        observations = sorted(observations, key=lambda o: o["_id"])
-    elif sort == "modified":
-        observations = sorted(observations, key=lambda o: arrow.get(o["modified"]).isoformat() if "modified" in o else "")[::-1]
-    elif sort == "modified-by":
-        observations = sorted(observations, key=lambda o: (0, o["modified-by"]) if "modified-by" in o else (1, None))
-    elif sort == "original-filename":
-        observations = sorted(observations, key=lambda o: [value.get("filename", None) for value in o.get("content", {}).values()])
-    elif sort == "tags":
-        observations = sorted(observations, key=lambda o: ("label:reviewed" in o["tags"], sorted(o["tags"])))
-    else:
-        flask.abort(400)
-    ids = [str(observation["_id"]) for observation in observations]
-    return flask.jsonify(observations=ids)
-
-
 @application.route("/observations/<exclude(count,tags):oid>", methods=["GET", "DELETE"])
 @require_auth
 def get_delete_observations_observation(oid):
