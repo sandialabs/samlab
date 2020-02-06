d00d7d8c14beaad9f3cf680176d24cc1c9f36e8c
Timothy M. Shead <tshead@sandia.gov>
2020-02-06 15:03:27 -0700

    Add functionality to display image directories in a notebook.

---
+++ samlab/notebook.py
@@ -0,0 +1,23 @@
+# Copyright 2018, National Technology & Engineering Solutions of Sandia, LLC
+# (NTESS).  Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
+
+"""Integration with Jupyter notebooks."""
+
+import logging
+
+import IPython.display
+
+
+log = logging.getLogger(__name__)
+
+
+def gallery(images, row_height="auto"):
+    content = []
+    content.append("<div style='display: flex; flex-flow: row wrap; text-align: center;'>")
+    for image in images:
+        content.append(f"<figure style='margin: 5px'><image title='{image}' src='{image}' style='height: {row_height}'/></figure>")
+    content.append("</div>")
+
+    return IPython.display.HTML("".join(content))
+
