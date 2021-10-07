f13145edee93caedf1fc1a62922c1609d52edd99
Timothy M. Shead <tshead@sandia.gov>
2021-10-06 18:37:22 -0600

    First iteration using gridstack for the dashboard.

--- samlab/dashboard/static/samlab-layout-test-widget.js
+++ samlab/dashboard/static/samlab-layout-test-widget.js
@@ -2,21 +2,26 @@
 // (NTESS).  Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
 // Government retains certain rights in this software.
 
-define(["knockout", "knockout.mapping"], function(ko, mapping)
+define(["debug", "knockout", "knockout.mapping"], function(debug, ko, mapping)
 {
     var component_name = "samlab-layout-test-widget";
+    var log = debug(component_name);
+
     ko.components.register(component_name,
     {
         viewModel:
         {
             createViewModel: function(widget, component_info)
             {
+                log("createViewModel", widget, component_info);
+
                 var component = mapping.fromJS({
-                    x: widget.x,
-                    y: widget.y,
-                    width: widget.width,
-                    height: widget.height,
                 });
+
+                component.x = widget.x;
+                component.y = widget.y;
+                component.width = widget.width;
+                component.height = widget.height;
 
                 component.state = widget.params.state;
 
