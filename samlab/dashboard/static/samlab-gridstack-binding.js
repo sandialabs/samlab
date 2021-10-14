784cfa4d8b043ca3b6b8ec350237322d94580128
Timothy M. Shead <tshead@sandia.gov>
2021-10-13 23:48:18 -0600

    Add missing copyright notice.

--- samlab/dashboard/static/samlab-gridstack-binding.js
+++ samlab/dashboard/static/samlab-gridstack-binding.js
@@ -1,3 +1,7 @@
+// Copyright 2018, National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS).  Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+
 define(["debug", "gridstack-h5", "knockout", "knockout.mapping"], function(debug, gridstack, ko, mapping)
 {
     var log = debug("samlab-gridstack-binding");
