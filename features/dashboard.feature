7d99d4bd0a049c6c9a391ec723bcf8beb7b00407
Timothy M. Shead <tshead@sandia.gov>
2021-10-18 21:45:32 -0600

    First iteration of a regression test using selenium.

--- features/dashboard.feature
+++ features/dashboard.feature
@@ -2,4 +2,5 @@ Feature: Dashboard
 
     Scenario:
         Given a running dashboard server
+        And a connected browser
 
--- features/steps/dashboard_steps.py
+++ features/steps/dashboard_steps.py
@@ -19,3 +19,24 @@ def step_impl(context):
     context.dashboard = samlab.dashboard.Server(config=False, coverage=True, quiet=True)
     context.add_cleanup(cleanup, context.dashboard)
     context.dashboard.ready(timeout=10)
+
+
+@given(u'a connected browser')
+def step_impl(context):
+    def cleanup(browser):
+        browser.close()
+
+    from selenium.webdriver import Firefox
+    from selenium.webdriver.common.by import By
+    from selenium.webdriver.firefox.options import Options
+    from selenium.webdriver.support.ui import WebDriverWait as Wait
+    from selenium.webdriver.support.expected_conditions import presence_of_element_located as element_located
+
+    options = Options()
+    options.headless = True
+
+    context.browser = Firefox(options=options)
+    context.browser.get(context.dashboard.uri)
+    context.browser_wait = Wait(context.browser, timeout=10)
+    context.browser_wait.until(element_located((By.ID, "samlab-notify-container")))
+
