c65d4ba818403ad06d0e60ea6c8eb9697606b7ce
Timothy M. Shead <tshead@sandia.gov>
2021-10-20 10:15:36 -0600

    Don't try to run coverage in behave, it can't be enabled before modules are loaded.

---
+++ regression.py
@@ -0,0 +1,17 @@
+# Copyright 2018, National Technology & Engineering Solutions of Sandia, LLC
+# (NTESS).  Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
+
+import glob
+import os
+import shutil
+import subprocess
+
+for path in glob.glob(".coverage*"):
+    os.remove(path)
+if os.path.exists(".cover"):
+    shutil.rmtree(".cover")
+subprocess.call(["coverage", "run", "--source", "samlab", "-m", "behave"])
+subprocess.call(["coverage", "combine"])
+subprocess.call(["coverage", "report"])
+subprocess.call(["coverage", "html", "--directory", ".cover"])
